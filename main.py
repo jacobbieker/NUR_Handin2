42c3083256428d81e381ceb1a231fa5f69dd1ec8
Jacob Bieker <jacob@bieker.tech>
2019-05-27 22:58:31 +0200

    Make main

---
+++ main.py
@@ -0,0 +1,26 @@
+from one import random_generator, all_one
+from part_two import part_two
+from part_three import part_three
+from four import all_four
+from part_six import part_six
+from part_seven import part_seven
+
+import sys
+
+sys.stdout = open('seed.txt', 'w')
+
+
+seed = 5227
+
+print("The seed for this project is: {}".format(seed))
+
+rand_gen = random_generator(seed)
+
+all_one(rand_gen)
+
+part_two(rand_gen)
+part_three()
+all_four(rand_gen)
+part_six()
+part_seven()
+
