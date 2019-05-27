f6e3737a57f270d0b318bd15eb0e77cdad651acf
Jacob Bieker <jacob@bieker.tech>
2019-05-28 01:51:54 +0200

    Add more to the report

--- main.py
+++ main.py
@@ -19,26 +19,20 @@ print("The seed for this project is: {}".format(seed))
 
 rand_gen = random_generator(seed)
 
-print("Start All One")
 all_one(rand_gen)
 plt.cla()
-print("End All One")
 part_two(rand_gen)
 plt.cla()
-print("End Part 2")
 
 # Now do multiprocessing as these can take awhile
 part_three()
 plt.cla()
-print("End Part 3")
 all_four()
 plt.cla()
-print("End Part 4")
 #part_six()
 plt.cla()
 part_seven()
 plt.cla()
-print("End Part 7")
 
 end = time.time()
 
