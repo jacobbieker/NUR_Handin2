b2b398c0038274beefc9ac02046434454ff0c255
Jacob Bieker <jacob@bieker.tech>
2019-05-28 00:04:34 +0200

    Remove unused code

--- part_seven.py
+++ part_seven.py
@@ -4,55 +4,6 @@ import matplotlib.pyplot as plt
 from matplotlib import patches
 
 
-class OctNode:
-    """Stores the data for an octree node, and spawns its children if possible"""
-
-    def __init__(self, center, size, masses, points, ids, leaves=[]):
-        self.center = center  # center of the node's box
-        self.size = size  # maximum side length of the box
-        self.children = []  # start out assuming that the node has no children
-
-        Npoints = len(points)
-
-        if Npoints == 1:
-            # if we're down to one point, we need to store stuff in the node
-            leaves.append(self)
-            self.COM = points[0]
-            self.mass = masses[0]
-            self.id = ids[0]
-            self.g = np.zeros(3)  # at each point, we will want the gravitational field
-        else:
-            self.GenerateChildren(points, masses, ids, leaves)  # if we have at least 2 points in the node,
-            # spawn its children
-
-            # now we can sum the total mass and center of mass hierarchically, visiting each point once!
-            com_total = np.zeros(3)  # running total for mass moments to get COM
-            m_total = 0.  # running total for masses
-            for c in self.children:
-                m, com = c.mass, c.COM
-                m_total += m
-                com_total += com * m  # add the moments of each child
-            self.mass = m_total
-            self.COM = com_total / self.mass
-            # print("{}, {}".format(self.mass, self.COM))
-
-    def GenerateChildren(self, points, masses, ids, leaves):
-        """Generates the node's children"""
-        octant_index = (points > self.center)  # does all comparisons needed to determine points' octants
-        for i in range(2):  # looping over the 8 octants
-            for j in range(2):
-                for k in range(2):
-                    in_octant = np.all(octant_index == np.bool_([i, j, k]), axis=1)
-                    if not np.any(in_octant): continue  # if no particles, don't make a node
-                    dx = 0.5 * self.size * (np.array([i, j, k]) - 0.5)  # offset between parent and child box centers
-                    self.children.append(OctNode(self.center + dx,
-                                                 self.size / 2,
-                                                 masses[in_octant],
-                                                 points[in_octant],
-                                                 ids[in_octant],
-                                                 leaves))
-
-
 class Particle(object):
     def __init__(self, mass, position, velocity, id, node=None):
         self.mass = mass
@@ -227,18 +178,13 @@ def part_seven():
 
     # Create Particles Only care about 2D in this example
     particles = []
-    print(len(coords))
     for index in range(len(coords[:])):
         particles.append(Particle(mass=masses[index], position=(coords[index][0], coords[index][1]),
                                   id=ids[index], velocity=velocities[index]))
 
     # Now have all the particles, add them to the tree
-    node = OctNode(masses=masses, points=coords, center=(75, 75, 75), size=150, ids=ids)
-    print(node.mass)
-    print(node.COM)
     BHTree = BHNode(center=(75, 75), length=150, particles=particles)
     BHTree.calc_multipole()
-    print(BHTree.moment)
 
     id_100 = particles[100].id
 
